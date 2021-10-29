5554916058c620a75a12ec039b499d6502e37a63
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-10-29 09:29:43 -0300

    fix problema colores

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -3,16 +3,16 @@ const fs = require('fs')
 const crearArchivoTabla = async (base = 5, hasta, listar) => {
   try {
     let salida = ''
-
-    console.log(listar)
+    let consola = ''
 
     for (let i = 0; i <= hasta; i++) {
-      salida += `${base} ${'x'.green} ${i} ${'='.green} ${base * i} \n`
+      salida += `${base} x ${i} = ${base * i} \n`
+      consola += `${base} ${'x'.green} ${i} ${'='.green} ${base * i} \n`
     }
 
     if (listar == true) {
       console.log(`<==========> Tabla del ${base} <==========> \n`.rainbow)
-      console.log(salida)
+      console.log(consola)
     }
 
     const nombreArchivo = `Tabla del ${base}.txt`
